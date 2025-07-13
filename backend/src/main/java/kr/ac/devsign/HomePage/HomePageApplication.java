9972ef54866bd01b50341b0e012ed1e85a824beb
YongJun Cho <whdydwns73@naver.com>
2025-07-13 23:14:50 +0900

    chore: 스프링부트 프로젝트 초기 세팅

---
+++ backend/src/main/java/kr/ac/devsign/HomePage/HomePageApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.devsign.HomePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomePageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/kr/ac/devsign/HomePage/HomePageApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.devsign.HomePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
