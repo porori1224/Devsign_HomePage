704d23b992ffafc94335297597b7cc990604095c
YongJun Cho <whdydwns73@naver.com>
2025-07-14 21:37:25 +0900

    refactor: ApiResponse파일 NoArgsConstructor 추가

--- backend/src/main/java/kr/ac/devsign/HomePage/dto/ApiResponse.java
+++ backend/src/main/java/kr/ac/devsign/HomePage/dto/ApiResponse.java
@@ -3,10 +3,12 @@ package kr.ac.devsign.HomePage.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ApiResponse<T> {
     private boolean success;
     private String message;
